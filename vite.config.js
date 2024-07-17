800c39dade1445e42e1a8cee038b8667f336e3a5
Gabby <gabbyesquibel@hotmail.com>
2024-07-17 02:12:28 -0600

    vercel error fix?

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
       'three': 'three',
       'three/examples/jsm': 'three/examples/jsm'
     }
+  },
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+      external: ['style.css']
+    },
+    chunkSizeWarningLimit: 1000 // Adjust the limit to a higher value
   }
 });
