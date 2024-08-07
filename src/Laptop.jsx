c9ea470d254a63c8108c15ea20f4d7a6ad4839ba
Gabsesq <77814862+Gabsesq@users.noreply.github.com>
2024-08-07 00:08:49 -0600

    Fixing for deployment

--- src/Laptop.jsx
+++ src/Laptop.jsx
@@ -1,29 +1,48 @@
-import { Html, useGLTF, Environment, PresentationControls } from "@react-three/drei";
-import { useLoader } from "@react-three/fiber";
+// @ts-nocheck
+import { Html, Environment, PresentationControls } from "@react-three/drei";
+import { useLoader, useThree } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useRef } from "react";
+import gsap from "gsap";
+
 
 export default function Laptop() {
-    const laptop = useLoader(GLTFLoader, "/NOKIA.glb"); // Adjust the path according to your file structure
-    const floor = useLoader(GLTFLoader, "/SCENE.glb");  // Load SCENE.glb
-    
+    const laptop = useLoader(GLTFLoader, "/NOKIA.glb");
+    const floor = useLoader(GLTFLoader, "/SCENE.glb");
+    const { camera } = useThree();
+    const initialPosition = useRef(camera.position.clone());
+
     // Rotate the laptop model if needed
     laptop.scene.rotation.x = Math.PI / 2;
+
+    const handleMouseEnter = () => {
+        console.log("Mouse entered iframe");
+        gsap.to(camera.position, {
+            x: -2,
+            y: 30,
+            z: 200,
+            duration: 1,
+            ease: "power2.inOut"
+        });
+    };
+
+    const handleMouseLeave = () => {
+        console.log("Mouse left iframe");
+        gsap.to(camera.position, {
+            x: initialPosition.current.x,
+            y: initialPosition.current.y,
+            z: initialPosition.current.z,
+            duration: 1,
+            ease: "power2.inOut"
+        });
+    };
 
     return (
         <>
             <Environment preset="warehouse" />
             <PresentationControls global polar={[-0.4, 0.2]}>
-                {/* Render the SCENE.glb model */}
-                <primitive 
-                    object={floor.scene} 
-                    position-y={-1.5} // Adjust position as needed
-                />
-
-                {/* Render the NOKIA.glb model */}
-                <primitive 
-                    object={laptop.scene} 
-                    position-y={-1}
-                >
+                <primitive object={floor.scene} position-y={-1.5} />
+                <primitive object={laptop.scene} position-y={-1}>
                     <Html
                         wrapperClass="laptop"
                         position={[.03, 1.15, -1.54]}
@@ -32,6 +51,8 @@ export default function Laptop() {
                         rotation-x={-1.6}
                         rotation-y={-.01}
                         rotation-z={-.001}
+                        onPointerEnter={handleMouseEnter}
+                        onPointerLeave={handleMouseLeave}
                     >
                         <iframe src="https://2-d-for-portfolio.vercel.app/" />
                     </Html>
